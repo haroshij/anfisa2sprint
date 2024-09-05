3b1c4a320686ac624f165673a05cb909b3991822
Stepan Pomogaibo <haroshij@yandex.ru>
2024-09-05 17:15:48 +0300

    create adminzone

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,8 @@
+# ice_cream/admin.py
 from django.contrib import admin
 
-# Register your models here.
+# Из модуля models импортируем модель Category...
+from .models import Category
+
+# ...и регистрируем её в админке:
+admin.site.register(Category)

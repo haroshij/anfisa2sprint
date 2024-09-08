9f5e0d8662f78e63a21a29ece3a3198cd096d7d5
Stepan Pomogaibo <haroshij@yandex.ru>
2024-09-08 16:06:12 +0300

    some progress

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -77,6 +77,16 @@ class IceCream(PublishedModel):
     toppings = models.ManyToManyField(
         Topping,
         verbose_name='Топпинги')
+
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
+
+    price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2)
+
     is_on_main = models.BooleanField(
         default=False,
         verbose_name='На главную')
@@ -84,6 +94,7 @@ class IceCream(PublishedModel):
     class Meta:
         verbose_name = 'мороженое'
         verbose_name_plural = 'Мороженое'
+        ordering = ('output_order', 'title')
 
     def __str__(self):
         return self.title
